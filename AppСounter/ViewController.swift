efaf46a6237466697e84e2e37c7029929510c603
Daria Granina <dashuny14@gmail.com>
2024-02-28 16:38:49 +0400

    Сделала кнопку счетчика для тестового задания к 3 спринту

--- AppСounter/ViewController.swift
+++ AppСounter/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var numb = 0
+    
+    @IBAction func tapOnButton(_ sender: Any) {
+        numb += 1
+        counterLabel.text = String(numb)
+    }
 }
 
